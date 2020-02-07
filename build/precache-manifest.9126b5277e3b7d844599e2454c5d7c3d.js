1dacfb14fc1e6c18eb1e9688c02d181ee53e6cbf
unknown <obrienules@gmail.com>
2020-02-07 11:59:38 +0100

    Run build

---
+++ build/precache-manifest.9126b5277e3b7d844599e2454c5d7c3d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1c760afbb74624def3f97828fff2a4",
+    "url": "/bettys-hub-frontend/index.html"
+  },
+  {
+    "revision": "e16cde8b961e98b43240",
+    "url": "/bettys-hub-frontend/static/css/main.fde58697.chunk.css"
+  },
+  {
+    "revision": "5a3a68f7340e31af4c9d",
+    "url": "/bettys-hub-frontend/static/js/2.0e42aeee.chunk.js"
+  },
+  {
+    "revision": "dfeea506dedb1f8ad64e8d3829765f8f",
+    "url": "/bettys-hub-frontend/static/js/2.0e42aeee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e16cde8b961e98b43240",
+    "url": "/bettys-hub-frontend/static/js/main.b407670d.chunk.js"
+  },
+  {
+    "revision": "fbd9bc547407b502baa0",
+    "url": "/bettys-hub-frontend/static/js/runtime-main.4b2474af.js"
+  },
+  {
+    "revision": "97f0ca3f32c9ee0ecd56281beecdb12a",
+    "url": "/bettys-hub-frontend/static/media/accessories.97f0ca3f.jpg"
+  },
+  {
+    "revision": "b43f1f6aa810b072449048599947c645",
+    "url": "/bettys-hub-frontend/static/media/add1.b43f1f6a.jpg"
+  },
+  {
+    "revision": "98c1dc2b33a3bdb4be3fd541db942367",
+    "url": "/bettys-hub-frontend/static/media/add2.98c1dc2b.jpg"
+  },
+  {
+    "revision": "630b80d38612ad752048f1b8a7e32052",
+    "url": "/bettys-hub-frontend/static/media/bg_low.630b80d3.jpg"
+  },
+  {
+    "revision": "70723a29e0360a33098cf82aafeb29f1",
+    "url": "/bettys-hub-frontend/static/media/fabric.70723a29.jpg"
+  },
+  {
+    "revision": "1fccb95152233abe974b12410f847644",
+    "url": "/bettys-hub-frontend/static/media/headoffice.1fccb951.jpg"
+  },
+  {
+    "revision": "664e147ef267635108a4d1aa55b3da0e",
+    "url": "/bettys-hub-frontend/static/media/image1.664e147e.jpg"
+  },
+  {
+    "revision": "99202acb7c49132d392ca98fe50a0a47",
+    "url": "/bettys-hub-frontend/static/media/image2.99202acb.png"
+  },
+  {
+    "revision": "edf5cee085de600483db2345dfe13a6d",
+    "url": "/bettys-hub-frontend/static/media/image2.edf5cee0.jpg"
+  },
+  {
+    "revision": "9d2f48dd281c5513d2a938246c599289",
+    "url": "/bettys-hub-frontend/static/media/image3.9d2f48dd.jpg"
+  },
+  {
+    "revision": "7df83fc7390ed029454ce264bcc634e7",
+    "url": "/bettys-hub-frontend/static/media/image4.7df83fc7.jpg"
+  },
+  {
+    "revision": "eed8f66af81ebf7cfc0c061c415e3c76",
+    "url": "/bettys-hub-frontend/static/media/image4.eed8f66a.png"
+  },
+  {
+    "revision": "74ce03cb1ecfe544facf3c23fdb44e1d",
+    "url": "/bettys-hub-frontend/static/media/image5.74ce03cb.jpg"
+  },
+  {
+    "revision": "b795743e1d3fbf9a5377012a303a730c",
+    "url": "/bettys-hub-frontend/static/media/image5.b795743e.png"
+  },
+  {
+    "revision": "89d8405a262a48a48fcc6e20f21b3c20",
+    "url": "/bettys-hub-frontend/static/media/image6.89d8405a.jpg"
+  },
+  {
+    "revision": "754276032d3a5099f93250a71cf07cc4",
+    "url": "/bettys-hub-frontend/static/media/image7.75427603.png"
+  },
+  {
+    "revision": "bbee634b79a9f95acb72990a9648f433",
+    "url": "/bettys-hub-frontend/static/media/image8.bbee634b.png"
+  },
+  {
+    "revision": "e934565919b8092b742dc16947ed9b12",
+    "url": "/bettys-hub-frontend/static/media/shopping-bag.e9345659.svg"
+  }
+]);
