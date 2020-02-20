0d1ef77a19261ceae54f59705b715919375eab20
unknown <obrienules@gmail.com>
2020-02-20 10:54:35 +0100

    Change to Hashrouter- react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
